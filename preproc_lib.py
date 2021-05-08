82a4ccd8f8dc1c3da4d4f9d0e870b25aa2c999bc
franciscobarber <45273569+franciscobarber@users.noreply.github.com>
2021-05-08 20:08:56 -0300

    Update preproc_lib.py

--- preproc_lib.py
+++ preproc_lib.py
@@ -8,10 +8,11 @@ import time
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from AE.spec_lib import pretty_spectrogram
-def create_specs(fft, time_long, step_size, log_ref):
+def create_specs(fft, time_long, step_size, log_ref, files_permutation):
   audio_dir='/content/free-spoken-digit-dataset/recordings/'
   file_names = [f for f in listdir(audio_dir) if isfile(join(audio_dir, f)) and '.wav' in f]
-  ms_list = np.zeros([0,int(fft/2),int(time_long/step_size)])
+  train_list = np.zeros([0,int(fft/2),int(time_long/step_size)])
+  test_list = np.zeros([0,int(fft/2),int(time_long/step_size)])
 
   #sp_sz=2046
   sp_sz = int(time_long)
@@ -28,11 +29,13 @@ def create_specs(fft, time_long, step_size, log_ref):
                                             ref=log_ref), axis=0)
     lms = np.split(ms, n_ms, axis=2)
     ms2 = np.concatenate(lms)
-    ms_list = np.append(ms_list,ms2,axis=0)
-  print(np.amin(ms_list))
-  print(np.amax(ms_list))  
-  return  train_test_split(
-    ms_list, test_size=0.20, random_state=42)
+    if files_permutation[i]<np.ceil(len(file_names)*0.2):
+      test_list = np.append(test_list,ms2,axis=0)   
+    else:
+      train_list = np.append(train_list,ms2,axis=0)
+    i += 1
+      return train_list, test_list
+
 class specData():
   def __init__(self,fft,time_long,fft_step_size_ratio,clip=1e0):
     self.fft= fft
